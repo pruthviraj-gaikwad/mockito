af6ea41299a431cdc96d0aa3d140147165402ae2
pruthviraj-gaikwad <pruthviraj162004@gmail.com>
2025-12-25 18:09:43 +0530

    learn annotation about mockito

--- mockito-demo/src/test/java/com/in28minutes/mockito/mockito_demo/business/SomeBusinessImplMockTest.java
+++ mockito-demo/src/test/java/com/in28minutes/mockito/mockito_demo/business/SomeBusinessImplMockTest.java
@@ -1,22 +1,30 @@
 package com.in28minutes.mockito.mockito_demo.business;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class SomeBusinessImplMockTest {
 	
+	@Mock
+	private DataService dataServiceMock;
 	
+	@InjectMocks
+	private SomeBusinessImpl someBusinessImpl;
 	
 	@Test
 	void findGreatestFrom_all() {
 
-		DataService dataServiceMock = mock(DataService.class);
+//		DataService dataServiceMock = mock(DataService.class);
 		//dataServiceMock.retriveAllData() => new int[] {23,11,33);
 		when(dataServiceMock.retriveAllData()).thenReturn(new int[] {25,33,22});
-		SomeBusinessImpl someBusinessImpl = new SomeBusinessImpl(dataServiceMock);
+//		SomeBusinessImpl someBusinessImpl = new SomeBusinessImpl(dataServiceMock);
 		int result = someBusinessImpl.findGreatestFromAllData();
 		assertEquals(25,result);
 	}
